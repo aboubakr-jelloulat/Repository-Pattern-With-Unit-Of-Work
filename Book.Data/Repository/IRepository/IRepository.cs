6e64017f85c2c509926932bc70fdf6e63670eb99
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-19 12:04:27 +0100

    Add entities in Repository

---
+++ Book.Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Book.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        Task AddAsync(T entity);
+
+
+
+        T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
+        Task<T?> GetAsync(Expression<Func<T, bool>> filterm, string? includeProperties = null, bool tracked = false);
+
+
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+
+        
+        void Remove(T entity);
+        Task RemoveAsync(T entity);
+
+
+        void RemoveRange(IEnumerable<T> entities);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
